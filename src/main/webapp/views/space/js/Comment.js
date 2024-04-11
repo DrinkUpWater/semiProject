2cfd57a8f3af80eedcc39eea6d6510c0c15a0dd6
hyeongwoo Shin <0416hj@naver.com>
2024-04-12 02:44:01 +0900

    수정:spaceDetail, 추가:test package:

--- src/main/webapp/views/space/js/Comment.js
+++ src/main/webapp/views/space/js/Comment.js
@@ -1,61 +1,3 @@
-
-function test(){
-    let commentTable=document.querySelector("#comment_table")
-    for(let i=0; i<2; i++){
-        commentTable.innerHTML+=
-        "<tr class='comment_list'>"
-            +"<th class='nickName'>hihi</th>"
-            + "<td class='mb-1' >user1</td>"
-         +"</tr>"
-    
-         +"<tr class='comment_list'>"
-                +"<th class='clear'></th>"
-                +"<td class='time'>시간데이터</td>"
-         +"</tr>"
-    
-         +"<tr class='host_replay_title'>"
-            +"<th class='clear'> </th>"
-            +"<td><button class='btn btn-link p-0 host-reply-toggle' data-target='#hostReply'>호스트답글</button></td>"
-         +"</tr>"
-    
-         +"<tr class='host_replay'>"
-             +"<th class='clear'> </th>"
-             +"<td> <div id='#hostReply' class='host-reply-content mt-2' style='display:none;'>"
-                +"<p>호스트 답글내용1</p>"
-             +"</div></td>"
-         +"</tr>"
-    
-    
-         +"<tr id='reply_info' class='comment_list'>"
-            +"<th class='clear'></th>"
-            +"<td>"
-                +"<div>답글</div>"
-                  +"<div id='reply div' style='width:100%; display:flex; justify-content: space-between;'>"
-                        +"<div 'style=width:100%;'>"
-                            + "<textarea placeholder='입력하세요' style='width:100%'></textarea>"
-                        +"</div>"
-    
-                        +"<div><button type='button'>등록하기</button></div>"
-    
-                    +"</div>"
-            +"</td>"
-    
-          +"</tr>"
-    
-          +"<tr id='comment_margin'>"
-             +"<td colspan='2' id='comment_line'><hr></td>"
-          +"</tr>"
-    
-    
-       
-        }
-
-
-
-        
-    
-}
-
 document.addEventListener('DOMContentLoaded', function () {
 
   
@@ -67,6 +9,8 @@ document.addEventListener('DOMContentLoaded', function () {
       
     }
 
+   // HostComment();
+
     $(".host-reply-toggle").click(function(){
         let targetId = $(this).data("target");
         console.log(this)
@@ -74,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
         $(targetId).toggle();
     });
 
-
+  
 });
 
 
@@ -84,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function insertCommentList(){
 
-    let sNo=document.querySelector("#spaceNum");//게시판번호 리퀘스트에서 받아온다.
+    let sNo=document.querySelector("#spaceNum");  //게시판번호 리퀘스트에서 받아온다.
     let con=document.querySelector("#content");
      $.ajax({
 
@@ -119,41 +63,42 @@ function insertCommentList(){
 
 
 
- function selectCommentList(){
+function selectCommentList(){
 
 
-        $.ajax({
+    $.ajax({
+
+        url:'comment.bo',
+        type:'GET',
     
-            url:'comment.bo',
-            type:'GET',
-        
-            data:{
-                spaceNum:1
-            },
+        data:{
+            spaceNum:1
+        },
 
-            success:function(response){
-            console.log(response);
+        success:function(response){
+        console.log(response);
 
-            let commentTable=document.querySelector("#comment_table")
-            commentList (commentTable,response) 
-                
-                
-            },
-            error:function(error){
-                console.log("error"+error);
+        let commentTable=document.querySelector("#comment_table")
+        commentList (commentTable,response) 
+            
+            
+        },
+        error:function(error){
+            console.log("error"+error);
 
-            }
+        }
 
 
-        })
+    })
 
-    }
+}
 
 
-function commentList(commentTable,response){
-   
+function commentList (commentTable,response){
+    let htmlContent=''
+
     for(let reply of response){
-        commentTable.innerHTML+=
+        htmlContent+=
         "<tr class='comment_list'>"
             +"<th class='nickName'>"+reply['userId']+"</th>"
             + "<td class='mb-1' >"+reply['commentContent']+"</td>"
@@ -166,12 +111,12 @@ function commentList(commentTable,response){
     
         +"<tr class='host_reply_title'>"
             +"<th class='clear'> </th>"
-            +"<td><button class='btn btn-link p-0  host-reply-toggle' data-target='#hostReply"+reply['hostReply']+"' >호스트답글</button></td>"
+            +"<td><button class='btn btn-link  p-0  host-reply-toggle' data-target='#hostReply' >호스트답글</button></td>"
         +"</tr>"
     
         +"<tr class='host_reply'>"
             +"<th class='clear'> </th>"
-            +"<td> <div  class='hostReplys' id='hostReply" + reply['commentNo'] + "' class='host-reply-content mt-2' style='display:block;'>"
+            +"<td> <div  class='hostReplys' id='hostReply' class='host-reply-content mt-2' style='display:block;'>"
                 +"<p>"+reply['hostReply']+"</p>"
             +"</div></td>"
         +"</tr>"
@@ -185,7 +130,7 @@ function commentList(commentTable,response){
                     +"<div style='width:100%;'>"
                          + "<textarea id='hostReplyContent' placeholder='입력하세요' style='width:100%'></textarea>"
                     +"</div>"
-                    +"<div><button type='button' onclick='submitHostReply()'>등록하기</button></div>"
+                    +"<div><button class='submitHostReplyBtn' type='button' '>등록하기</button></div>"
                     
                 +"</div>"
             +"</td>"
@@ -196,28 +141,25 @@ function commentList(commentTable,response){
         +"<tr id='comment_margin'>"
             +"<td colspan='2' id='comment_line'><hr></td>"
         +"</tr>"
-    
+     
 
     
 
     }
+    commentTable.innerHTML=htmlContent
 
-
-   // 등록하기 버튼에 이벤트 리스너 추가
-    let submitHostReplyButtons = document.querySelectorAll('.submitHostReplyBtn');
-    submitHostReplyButtons.forEach(button => {
-        button.addEventListener('click', submitHostReply);
-    });
-
-    // submitHostReply 함수 정의
-    function submitHostReply() {
-        let replyNo = this.closest('tr').previousElementSibling.querySelector('.host-reply-toggle').dataset.target.split('hostReply')[1];
-        let replyContent = this.closest('tr').querySelector('.hostReplyContent').value;
-        let url = '/hostreply.re?replyNo=' + replyNo + '&replyContent=' + encodeURIComponent(replyContent);
-        location.href = url;
-    }
+ 
 
 }
 
-
+function HostComment() {
+    document.addEventListener('click', function(event) {
+        if (event.target.classList.contains('submitHostReplyBtn')) {
+            let replyNo = event.target.closest('tr').previousElementSibling.querySelector('.host-reply-toggle').dataset.target.replace('hostReply', '');
+            let replyContent = event.target.closest('tr').querySelector('.hostReplyContent').value;
+            let url = `/hostreply.re?replyNo=${replyNo}&replyContent=${encodeURIComponent(replyContent)}`;
+            location.href = url;
+        }
+    });
+}
 
